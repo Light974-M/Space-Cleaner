ebe8768af160c50f8838eb0f5ec72bdee629d9ad
kagnous <antoinecasa34@gmail.com>
2021-05-05 10:15:26 +0200

    VER 1.1.5.A
    route avec NavMesh et changement de voie

--- Assets/Scripts/SpaceShip/SpaceShipController.cs
+++ Assets/Scripts/SpaceShip/SpaceShipController.cs
@@ -1,33 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpaceShipController : MonoBehaviour
 {
-    public float speed;
+    // Trajectoire
     public Transform[] waypoints;
-
+    // Point actuel visé
     public Transform target;
+    // Index du point visé
     private int destPoint = 0;
+    // Vitesse du véhicule
+    private float defaultSpeed;
+
+    private NavMeshAgent navMesh;
+    bool lookIsActive;
 
     void Start()
     {
+        lookIsActive = true;
         target = waypoints[0];
+
+        navMesh = GetComponent<NavMeshAgent>();
+        defaultSpeed = navMesh.speed;
+        GoTo(target);
+        transform.LookAt(target);
     }
 
     void Update()
     {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if(lookIsActive)
+        {
+            transform.LookAt(target);
+        }
+        // Version avec Translate
+        //Vector3 dir = target.position - transform.position;
+        //transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
         //Si le vaisseau est presque arrivé à sa target
-        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        if (Vector3.Distance(transform.position, target.position) < 2f)
         {
             // Si la target n'est pas la dernière de la liste
-            if(target != waypoints[waypoints.Length-1])
+            if (target != waypoints[waypoints.Length - 1])
             {
+                StartCoroutine(CoroutineStopLook());
                 destPoint = destPoint + 1;
                 target = waypoints[destPoint];
+                GoTo(target);
+                
+                if (navMesh.speed != defaultSpeed)
+                {
+                    navMesh.speed = defaultSpeed;
+                }
             }
             else
             {
@@ -39,16 +64,27 @@ public class SpaceShipController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Debug.Log("Collision");
+        Debug.Log("Hit");
         if (collision.gameObject.layer == LayerMask.NameToLayer("SpaceShip"))
         {
-            //Debug.Log("détecté");
-            SpaceShipController frontShipController = collision.gameObject.GetComponent<SpaceShipController>();
-            if(frontShipController.speed < speed)
+            NavMeshAgent navMeshOtherShip = collision.collider.gameObject.GetComponent<NavMeshAgent>();        
+            if (navMeshOtherShip.speed < navMesh.speed)
             {
-                speed = frontShipController.speed;
-                //Debug.Log("appliqué");
+                navMesh.speed = navMeshOtherShip.speed;
+                //StartCoroutine(SlowdownSpeed(navMeshOtherShip.speed));
             }
         }
+    }
+
+    public void GoTo(Transform target)
+    {
+        navMesh.SetDestination(target.position);
+    }
+
+    IEnumerator CoroutineStopLook()
+    {
+        lookIsActive = false;
+        yield return new WaitForSeconds(1.25f);
+        lookIsActive = true;
     }
 }
--- Assets/Scripts/SpaceShip/SpawnShip.cs
+++ Assets/Scripts/SpaceShip/SpawnShip.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class SpawnShip : MonoBehaviour
 {
     public List<GameObject> spaceShips;
+    // Liste des itinéraires possibles
+    //public List<Transform[]> roads;
     //public Transform target;
-    public Transform[] waypoints;
+    public Transform[] waypoints1;
+    public Transform[] waypoints2;
 
     //Counter Vaisseau
-    public float timeBeforeGenerateSpaceShip = 3;
+    private float timeBeforeGenerateSpaceShip = 3;
     float currentTimeBeforeNextGenSpaceShip = 0;
+    public float tempsMin = 1;
+    public float tempsMax = 5;
 
     void Update()
     {
+        timeBeforeGenerateSpaceShip = Random.Range(tempsMin,tempsMax);
+
         //Augmenter le timer du pop de vaisseau
         currentTimeBeforeNextGenSpaceShip += Time.deltaTime;
 
@@ -34,8 +41,17 @@ public class SpawnShip : MonoBehaviour
             //On fait une copie du vaisseau
             GameObject newSpaceShip = Instantiate(gameObject, transform);
             SpaceShipController spaceShipController = newSpaceShip.GetComponent<SpaceShipController>();
-            //spaceShipController.target = target;
-            spaceShipController.waypoints = waypoints;
+            int var = Random.Range(1, 3);
+            //Debug.Log(var);
+            if ( var == 1)
+            {
+                spaceShipController.waypoints = waypoints1;
+            }
+            else
+            {
+                spaceShipController.waypoints = waypoints2;
+            }
+            //spaceShipController.waypoints = roads[Random.Range(0, roads.Count)];
         }
     }
 }
