53d08b00276e718ea50dd6f519ca3d89afead21a
kagnous <antoinecasa34@gmail.com>
2021-05-12 15:40:59 +0200

    VER 2.0.3.A
    Maj son / audiomanager

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioSource audioSourceMusic;
+    public AudioSource audioSourceSound;
+    // Musique jouée in game
+    public AudioClip music; 
+    // Différents buitages joués
+    [Header("Bruitages :")]
+    public AudioClip x;
+    public AudioClip y;
+    public AudioClip z;
+
+    void Start()
+    {
+        audioSourceMusic.clip = music;
+        audioSourceMusic.Play();
+    }
+
+    public void PlaySong()
+    {
+        audioSourceSound.PlayOneShot(x);
+    }
+}
