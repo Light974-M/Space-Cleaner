e43a00b353c69c5b76bd804de31ca8fdcca3fb91
kagnous <antoinecasa34@gmail.com>
2021-05-17 09:56:33 +0200

    VER 2.1.3.A
    proto skybox et ajout script nouvelle route

---
+++ Assets/Scripts/SpaceShip/SpaceShipControllerV2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpaceShipControllerV2 : MonoBehaviour
+{
+    // Point visé
+    public Transform target;
+    // Vitesse du véhicule
+    public float defaultSpeed;
+    public float speed;
+
+    void Start()
+    {
+        transform.LookAt(target);
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        transform.LookAt(target);
+        if (Vector3.Distance(transform.position, target.position) < 2f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("SpaceShip"))
+        {
+            SpaceShipControllerV2 otherSpaceShipController = collision.collider.gameObject.GetComponent<SpaceShipControllerV2>();
+            if (otherSpaceShipController.speed < speed)
+            {
+                speed = otherSpaceShipController.speed;
+                Debug.Log("Hit");
+            }
+        }
+    }
+}
